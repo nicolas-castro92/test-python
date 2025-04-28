6e6644a859b83abc96446ecfc9a34e5465b0bcf9
nicolas-castro92 <77744134+nicolas-castro92@users.noreply.github.com>
2025-04-28 12:18:02 -0500

    Update test_calculos.py

--- test_calculos.py
+++ test_calculos.py
@@ -3,11 +3,11 @@ from calculos import raiz_cuadrada
 
 class TestCalculos(unittest.TestCase):
     def test_raiz_cuadrada_valor_positivo(self):
-        self.assertEqual(raiz_cuadrada(7), 3.0)
-        self.assertEqual(raiz_cuadrada(16), 4.0)
+        self.assertEqual(raiz_cuadrada(7), 3)
+        self.assertEqual(raiz_cuadrada(16), 4)
 
     def test_raiz_cuadrada_cero(self):
-        self.assertEqual(raiz_cuadrada(0), 0.0)
+        self.assertEqual(raiz_cuadrada(0), 0)
 
     def test_raiz_cuadrada_error_negativo(self):
         with self.assertRaises(ValueError):
